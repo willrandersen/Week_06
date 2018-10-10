8cf679faa0f5c770349d4ac2bea3ce00b0929f15
arshiam2 <arshiam2@illinois.edu>
2018-10-10 14:47:18 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -9,7 +9,31 @@ class App extends Component {
       "Add boba",
       "Drain water",
       "Add tea"
-    ]
+    ],
+    edits: []
+  };
+
+  handleChange = todo => event => {
+    let updatedEdits = this.state.edits;
+    updatedEdits[this.state.todos.indexOf(todo)] = event.target.value;
+    this.setState({
+      edits: updatedEdits
+    });
+
+    console.log(this.state.edits);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const index = this.state.todos.indexOf(event.target.value);
+    let newTodos = this.state.todos;
+    newTodos[index] = this.state.edits[index];
+    let updatedEdits = this.state.edits;
+    updatedEdits[index] = "";
+    this.setState({
+      todos: newTodos,
+      edits: updatedEdits
+    });
   };
 
   render() {
@@ -17,7 +41,13 @@ class App extends Component {
       <div>
         <h1> How to make boba at home</h1>
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+          <li key={todo}>
+            {todo + " "}
+            <input type="text" onChange={this.handleChange(todo)} />
+            <button value={todo} onClick={this.handleSubmit}>
+              Update
+            </button>
+          </li>
         ))}
       </div>
     );
