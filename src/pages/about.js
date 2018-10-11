99d4972ec3adec856af95df013d4c512f2126a31
Will Andersen <wra3@illinois.edu>
2018-10-10 19:58:18 -0500

    Attempted to creat components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 
 class About extends React.Component {
+
+  state = {
+      newTodo: ""
+    };
+
+  handleSubmit = event => {
+      event.preventDefault()
+      this.setState({
+        todos: [...this.state.todos, this.state.newTodo],
+        newTodo: ""
+      });
+    };
+
+    handleChange = event => {
+      this.setState({ newTodo: event.target.value });
+    };
+
   render() {
     return (
       <div>
         <h2>About</h2>
+
+        <input
+              type="text"
+              value={this.state.newTodo}
+              onChange={this.handleChange}
+            />
+            <button onClick={this.handleSubmit()}>EDIT</button>
+
       </div>
     );
   }
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,58 @@
 import React, { Component } from "react";
 
 class Home extends React.Component {
-  render() {
-    return ( <div>
-        <h2>Home</h2>
-      </div>);
+  state = {
+      newTodo: "",
+      todos: [
+        "Buy tea",
+        "Buy boba",
+        "Boil water",
+        "Add boba",
+        "Drain water",
+        "Add tea"
+      ]
+    };
+
+    handleChange = event => {
+        this.setState({ newTodo: event.target.value });
+      };
+       handleSubmit = event => {
+        event.preventDefault()
+        this.setState({
+          todos: [...this.state.todos, this.state.newTodo],
+          newTodo: ""
+        });
+      };
+
+      handleDelete = event => {
+        var newTodoList = [...this.state.todos];
+        var index = newTodoList.indexOf(event.target.value);
+        newTodoList.splice(index, 1);
+        this.setState({todos: newTodoList});
+      };
+
+
+        handleChangeName = event => {
+            console.log("DELETE");
+          };
+
+    render() {
+      return (
+        <div>
+          <h1> How to make boba at home</h1>
+          {this.state.todos.map(todo => (
+            <div>
+             <li key={todo}>
+               {todo + "  "}
+
+             </li>
+           </div>
+          ))}
+
+
+        </div>
+      );
+    }
   }
-}
 
 export default Home;
